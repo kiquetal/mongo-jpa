5272589981279c4dca55599a71ccdb9cba076e0b
kiquetal <kiquetal@gmail.com>
2024-03-11 09:45:33 -0300

    Adding liquibase for database migratoin

--- src/main/java/me/cresterida/CreatorEntity.java
+++ src/main/java/me/cresterida/CreatorEntity.java
@@ -4,6 +4,7 @@ package me.cresterida;
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
 import io.quarkus.mongodb.panache.common.MongoEntity;
 
+
 @MongoEntity(collection = "users")
 public class CreatorEntity extends PanacheMongoEntity
 {
